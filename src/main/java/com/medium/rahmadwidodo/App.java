bf2daa57b9a5b1c2b5aebe366e3bb8da03e1af52
Rahmad Widodo <rahmadwidodo1994@gmail.com>
2019-02-12 22:04:09 +0700

    tambahan data

--- src/main/java/com/medium/rahmadwidodo/App.java
+++ src/main/java/com/medium/rahmadwidodo/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	System.out.println( "Rahmad Widodo" );
+	System.out.println( "Testing Maven With Jenkins" );
     }
 }
